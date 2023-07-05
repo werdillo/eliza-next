6417bf6d478a7441c9a5572b48efdfac2026e873
werdillo <55383654+werdillo@users.noreply.github.com>
2023-07-05 19:21:30 +0300

    comment

--- app/[locale]/page.js
+++ app/[locale]/page.js
@@ -1,7 +1,7 @@
 
 import { Inter } from 'next/font/google'
 import SimpleSlider from '@/components/SimpleSlider'
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Eliza-k premium furniture',
